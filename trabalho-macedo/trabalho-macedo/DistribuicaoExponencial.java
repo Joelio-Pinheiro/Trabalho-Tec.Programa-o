7b181d4509f9261140295afadf762c6eca71839b
Luiz Eduardo <123267675+Luyami@users.noreply.github.com>
2024-07-18 08:51:31 -0300

    Delete trabalho-macedo/trabalho-macedo directory

--- trabalho-macedo/trabalho-macedo/DistribuicaoExponencial.java
+++
@@ -1,3 +0,0 @@
-public class DistribuicaoExponencial extends Distribuicao {
-    
-}
--- trabalho-macedo/trabalho-macedo/Estatisticas.java
+++
@@ -1,7 +0,0 @@
-public class Estatisticas<T extends Distribuicao> {
-    public void imprimirValoresGerados(int quantidade_valores){
-        if (quantidade_valores < 0){
-            throw new Exception("Quantidade de valores deve ser um número positivo");
-        }
-    }
-}
