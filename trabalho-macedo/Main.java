b1464bc28760d8702359c2205916e1641cbef96e
Luiz Eduardo <123267675+Luyami@users.noreply.github.com>
2024-07-18 14:04:48 -0300

    Update Main.java

--- trabalho-macedo/Main.java
+++ trabalho-macedo/Main.java
@@ -2,15 +2,27 @@ public class Main {
     public static void main(String[] args) throws Exception {
 
         DistribuicaoNormal distribuicaoNormal = new DistribuicaoNormal(0, 1);
-
+        DistribuicaoUniforme distribuicaoUniforme = new DistribuicaoUniforme(0, 1);
+        DistribuicaoPoisson distribuicaoPoisson = new DistribuicaoPoisson(2);
 
         Estatisticas<DistribuicaoNormal> estatisticasNormal = new Estatisticas<>(distribuicaoNormal);
-  
-        System.out.println(distribuicaoNormal.calcularDensidade(0));
+        Estatisticas<DistribuicaoUniforme> estatisticasUniforme = new Estatisticas<>(distribuicaoUniforme);
+        Estatisticas<DistribuicaoPoisson> estatisticasPoisson = new Estatisticas<>(distribuicaoPoisson);
+
         System.out.println("Distribuição Normal:");
         estatisticasNormal.imprimirValoresGerados(5);
         estatisticasNormal.imprimirMedia();
         estatisticasNormal.imprimirVariancia();
+
+        System.out.println("\nDistribuição Uniforme:");
+        estatisticasUniforme.imprimirValoresGerados(5);
+        estatisticasUniforme.imprimirMedia();
+        estatisticasUniforme.imprimirVariancia();
+
+        System.out.println("\nDistribuição Poisson:");
+        estatisticasPoisson.imprimirValoresGerados(5);
+        estatisticasPoisson.imprimirMedia();
+        estatisticasPoisson.imprimirVariancia();
     }
 }
 
