aa779afb25196dd6ddd6cf447465270b2a539eb7
Joelio-Pinheiro <joeliopinheiro10@gmail.com>
2024-07-18 10:45:53 -0300

    feat: add distribution functions

--- trabalho-macedo/Distribuicao.java
+++ trabalho-macedo/Distribuicao.java
@@ -1,11 +1,39 @@
 public abstract class Distribuicao {
-    public double media(){return 0.0f;}
+    public static double media(double list[]){
+        double tam = list.length;
+        double sum = 0;
+        for (double d : list) {
+            sum += d;
+        }
+        double media = sum/tam;
+        return media;
+    }
     
-    public double variancia_amostral(){return 0.0f;}
-    public double variancia_populacional(){return 0.0f;}
+    public static double variancia_amostral(double list[]){
+        double media = media(list);
+        double sum = 0;
+        for (double d : list) {
+            sum += Math.pow((d-media), 2);
+        }
+        double var = sum/(list.length-1);
+        return var;
+    }
+    public static double variancia_populacional(double list[]){
+        double media = media(list);
+        double sum = 0;
+        for (double d : list) {
+            sum += Math.pow((d-media), 2);
+        }
+        double var = sum/list.length;
+        return var;
+    }
 
-    public double desviopadrao_amostral(){return Math.sqrt(variancia_amostral());}
-    public double desviopadrao_populacional(){return Math.sqrt(variancia_populacional());}
+    public static double desviopadrao_amostral(double list[]){
+        return Math.sqrt(variancia_amostral(list));
+    }
+    public static double desviopadrao_populacional(double list[]){
+        return Math.sqrt(variancia_populacional(list));
+    }
 
     //... Métodos abstratos comuns às distribuições
 }
