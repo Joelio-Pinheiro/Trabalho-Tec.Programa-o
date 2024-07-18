8226469f5ccac4b11aee534b65d7bd9602afd1c1
Luiz Eduardo <123267675+Luyami@users.noreply.github.com>
2024-07-18 09:57:42 -0300

    Update Distribuicao.java

--- trabalho-macedo/Distribuicao.java
+++ trabalho-macedo/Distribuicao.java
@@ -1,39 +1,20 @@
 public abstract class Distribuicao {
-    public static double media(double list[]){
-        double tam = list.length;
-        double sum = 0;
-        for (double d : list) {
-            sum += d;
-        }
-        double media = sum/tam;
-        return media;
-    }
-    
-    public static double variancia_amostral(double list[]){
-        double media = media(list);
-        double sum = 0;
-        for (double d : list) {
-            sum += Math.pow((d-media), 2);
-        }
-        double var = sum/(list.length-1);
-        return var;
-    }
-    public static double variancia_populacional(double list[]){
-        double media = media(list);
-        double sum = 0;
-        for (double d : list) {
-            sum += Math.pow((d-media), 2);
-        }
-        double var = sum/list.length;
-        return var;
-    }
+    //Limites delimitadores das densidades mais relevantes da distribuição
+    protected double limiteInferior;
+    protected double limiteSuperior;
 
-    public static double desviopadrao_amostral(double list[]){
-        return Math.sqrt(variancia_amostral(list));
-    }
-    public static double desviopadrao_populacional(double list[]){
-        return Math.sqrt(variancia_populacional(list));
-    }
+    protected double media;
+    protected double desvio;
+
+    public double getMedia(){return this.media;}
+    public double getDesvio() {return this.desvio;}
+    public double getVariancia() {return Math.pow(this.desvio, 2);}
+
+    public double getLimiteInferior() {return this.limiteInferior;}
+    public double getLimiteSuperior() {return this.limiteSuperior;}
 
     //... Métodos abstratos comuns às distribuições
+    public abstract double calcularDensidade(double x);
+    public abstract double calcularMedia();
+    public abstract double calcularVariancia();
 }
