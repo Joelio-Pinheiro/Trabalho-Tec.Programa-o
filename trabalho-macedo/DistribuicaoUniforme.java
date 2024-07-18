af9a6069121c50eefd8b51bc4ec24e7d1d36e89d
Luiz Eduardo <123267675+Luyami@users.noreply.github.com>
2024-07-18 12:01:47 -0300

    Update DistribuicaoUniforme.java

--- trabalho-macedo/DistribuicaoUniforme.java
+++ trabalho-macedo/DistribuicaoUniforme.java
@@ -1,3 +1,26 @@
 public class DistribuicaoUniforme extends Distribuicao {
-    
+    public DistribuicaoUniforme(double limiteInferior, double limiteSuperior){
+        this.limiteInferior = limiteInferior;
+        this.limiteSuperior = limiteSuperior;
+    }
+    public DistribuicaoUniforme(){
+        this.limiteInferior = 0;
+        this.limiteSuperior = 1;
+    }
+
+    public double calcularDensidade(double x){
+        double comprimento = this.limiteSuperior - this.limiteInferior;
+
+        return 1/comprimento;
+    }
+
+    public double calcularMedia(){
+        return (this.limiteSuperior + this.limiteInferior)/2;
+    }
+
+    public double calcularVariancia(){
+        double comprimento_quadrado = Math.pow(this.limiteSuperior - this.limiteInferior, 2);
+
+        return Math.sqrt(comprimento_quadrado/12);
+    }
 }
