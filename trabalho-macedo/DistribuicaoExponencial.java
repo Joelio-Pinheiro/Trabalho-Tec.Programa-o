2e5fc675a5a82db73e885d51b6b1467eeacf88af
Lukizou <luiz0312@alu.ufc.br>
2024-07-18 11:55:48 -0300

    Update DistribuicaoExponencial.java

--- trabalho-macedo/DistribuicaoExponencial.java
+++ trabalho-macedo/DistribuicaoExponencial.java
@@ -1,42 +1,50 @@
 import java.util.Random;
-public class DistribuicaoExponencial extends Distribuicao{
+public class Teste{
     
     Random random = new Random();
 
-    int[] array = new int[5];
+    float[] array = new float[5];
     int lambda;
-
-    DistribuicaoExponencial(int x){
-        lambda = x;
+    
+    Teste(int x){
+        this.lambda = x;
     }
 
     double calcVariancia(){
-        double variancia = 1.0/(lambda * lambda);
+        double variancia = 1.0/(this.lambda * this.lambda);
 
         return variancia;
     }
 
     void imprimirValores(){
         double exp = Math.E;
+
         System.out.println("Os valores do vetor são: ");
+
         for(int i = 0; i < 5; i++){
-            array[i] = random.nextInt();
+            this.array[i] = random.nextFloat();
 
             System.out.print(array[i]+",");
         }
-        System.out.println("Os valores gerados são: ");
-        for(int i = 0; i < 5; i++){
-            double pot = lambda * array[i];
-            double val = lambda * Math.pow((1/exp), pot);
 
-            System.out.print(val+" ");
+        System.out.println("\nOs valores gerados são: ");
+
+        for(int i = 0; i < 5; i++){
+            double pot = this.lambda * this.array[i];
+            double val = this.lambda * Math.pow((1/exp), pot);
+            if(i == 4){
+                System.out.print(val);
+            }
+            else{
+                System.out.print(val+",");
+            }
+            
         }
     }
+
     void imprimirVariancia(){
         double varianciaEsp = calcVariancia();
 
-        System.out.println("O valor da variância é: "+varianciaEsp);
+        System.out.println("\nO valor da variância é: "+varianciaEsp);
     }
-
-
 }
