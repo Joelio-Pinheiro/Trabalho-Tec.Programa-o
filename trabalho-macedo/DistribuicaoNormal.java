66b0acfb2e72161fe2f46868dd3c07646edc57f1
Luiz Eduardo <123267675+Luyami@users.noreply.github.com>
2024-07-18 14:04:02 -0300

    Update DistribuicaoNormal.java

--- trabalho-macedo/DistribuicaoNormal.java
+++ trabalho-macedo/DistribuicaoNormal.java
@@ -1,10 +1,10 @@
 public class DistribuicaoNormal extends Distribuicao {
     public DistribuicaoNormal(double media, double desvio){
-        this.media = media;
-        this.desvio = desvio;
-
         this.limiteInferior = this.media - this.desvio*Math.PI;
         this.limiteSuperior = this.media + this.desvio*Math.PI;
+
+        this.media = media;
+        this.desvio = desvio;
     }
     public DistribuicaoNormal(double media){
         this(media, 1);
@@ -13,7 +13,7 @@ public class DistribuicaoNormal extends Distribuicao {
         this(0, 1);
     }
 
-    public double calcularDensidade(double x){
+    public double calcularDistribuicao(double x){
         double denominador = Math.sqrt(2*Math.PI) * this.getDesvio();
         double euler_expoente = -Math.pow(x - this.getMedia(), 2)/(2*Math.pow(this.getDesvio(), 2));
 
@@ -22,6 +22,6 @@ public class DistribuicaoNormal extends Distribuicao {
         return resultado;
     }
 
-    public double calcularMedia() { return this.getMedia(); }
-    public double calcularVariancia() { return this.getVariancia(); }
+    protected double calcularMedia() { return this.getMedia(); }
+    protected double calcularDesvio() { return this.getDesvio(); }
 }
