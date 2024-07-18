0969a917524a189b456da12568e762de316cfcf5
Luiz Eduardo <123267675+Luyami@users.noreply.github.com>
2024-07-18 09:59:45 -0300

    Update Estatisticas.java

--- trabalho-macedo/Estatisticas.java
+++ trabalho-macedo/Estatisticas.java
@@ -1,3 +1,39 @@
+import java.util.Random;
+
 public class Estatisticas<T extends Distribuicao> {
-    //Métodos para cálculo das distribuições...
+    T distribuicao;
+
+    public Estatisticas(T distribuicao){
+        this.distribuicao = distribuicao;
+    }
+
+    public void imprimirValoresGerados(int quantidade_valores) throws Exception{
+        if (quantidade_valores < 0){
+            throw new Exception("Quantidade de valores deve ser um número positivo!");
+        }
+
+        String resultado = "{ ";
+        Random rng = new Random();
+        for (int i = 0; i < quantidade_valores; i++){
+            double randomX = distribuicao.getLimiteInferior() + (distribuicao.getLimiteSuperior() - distribuicao.getLimiteInferior()) * rng.nextDouble();;
+            double densidade = distribuicao.calcularDensidade(randomX);
+
+            resultado += densidade + ", ";
+        }
+        resultado += "}";
+
+        System.out.println("Os valores gerados são: " + resultado);
+    }
+
+    public void imprimirMedia(){
+        double media = distribuicao.calcularMedia();
+
+        System.out.println("A média calculada é: " + media);
+    }
+
+    public void imprimirVariancia(){
+        double variancia = distribuicao.calcularVariancia();
+
+        System.out.println("A variância calculada é: " + variancia);
+    }
 }
